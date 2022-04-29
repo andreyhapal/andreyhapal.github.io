3ae8b671d003826ee5bd6343348bb24147376065
andreyhapal <andreyrapterror@mail.ru>
2022-04-30 00:49:24 +0400

    products page init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
