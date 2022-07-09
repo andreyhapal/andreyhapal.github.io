378066fdfa5009df2576814f29c75f4b43e9c72a
andreyhapal <andreyrapterror@mail.ru>
2022-07-10 02:51:40 +0400

    rework product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { ProductsComponent } from './products/products.component';
+import { ProductCardComponent } from './products/product-card/product-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    ProductsComponent
+    ProductsComponent,
+    ProductCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
